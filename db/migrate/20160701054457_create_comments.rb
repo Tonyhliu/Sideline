eeac11f41b41266e534605f8088d99184f93b938
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-06-30 22:46:43 -0700

    comments schema

--- db/migrate/20160701054457_create_comments.rb
+++ db/migrate/20160701054457_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :body, null: false, default: ""
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701053849) do
+ActiveRecord::Schema.define(version: 20160701054457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body",       default: "", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "stories", force: :cascade do |t|
     t.integer  "user_id",                    null: false
