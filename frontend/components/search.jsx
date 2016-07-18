b0ab7fe359657cd5663845b68a5b58b8196473c8
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-07-18 10:37:20 -0700

    search bar modified

--- frontend/components/search.jsx
+++ frontend/components/search.jsx
@@ -62,7 +62,6 @@ const Search = React.createClass({
     e.preventDefault();
     this.setState({ query: '' });
     hashHistory.push(`/stories/${e.target.value}`);
-    StoryActions.fetchAllStories({ query: '' });
   },
 
   _searchClick() {
