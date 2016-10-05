53207646da0dff8f2582a4e3d63a1442cb2d0f3b
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-10-04 17:09:14 -0700

    added table to display data

--- frontend/components/stats.jsx
+++ frontend/components/stats.jsx
@@ -1,31 +1,73 @@
 const React = require('react');
 const StatsApiUtil = require('../util/stats_api_util');
 const StatsItem = require('./stats_item');
+const Button = require('react-bootstrap').Button;
+const Table = require('react-bootstrap').Table;
 
 const Stats = React.createClass({
   getInitialState() {
-    return({ stats: [] });
+    return({ nbaStats: [],
+              nflStats: [] });
   },
 
-  _fetchStats(e) {
+  _fetchNbaStats(e) {
     e.preventDefault();
-    StatsApiUtil.getNbaStats(this._updateStats);
+    StatsApiUtil.getNbaStats("nbaStats", this._updateStats);
   },
 
-  _updateStats(resp) {
-    this.setState({ stats: resp });
+  _fetchNflStats(e) {
+    e.preventDefault();
+    StatsApiUtil.getNflStats("nflStats", this._updateStats);
+  },
+
+  _updateStats(typeOfStats, resp) {
+    this.setState({ [typeOfStats]: resp });
   },
 
   render() {
+    // <h1 onClick={this._fetchNflStats}>
+    //   <Button className="fetch-fl-players">
+    //     Fetch NFL Players
+    //   </Button>
+    // </h1>
+    // debugger
     return (
       <div>
-        <h1 onClick={this._fetchStats}>Fetch NBA Players</h1>
-        <ul>
-          {this.state.stats.map(el => {
-            return <StatsItem key={el.PlayerID}
-                              player={el} />;
-          })}
-        </ul>
+        <h1 onClick={this._fetchNbaStats}>
+          <Button className="fetch-nba-players">
+            Fetch NBA Players
+          </Button>
+        </h1>
+        <Table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Team</th>
+              <th>Jersey Number</th>
+              <th>Height (in inches)</th>
+              <th>Experience (in years)</th>
+              <th>Position</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.nbaStats.map(el => {
+              return (<tr key={el.PlayerID}>
+                <td><img className="stats-item-pic"
+                          src={el.PhotoUrl}>
+                </img></td>
+                <td>{el.FirstName}</td>
+                <td>{el.LastName}</td>
+                <td>{el.Team}</td>
+                <td>{el.Jersey}</td>
+                <td>{el.Height}</td>
+                <td>{el.Experience}</td>
+                <td>{el.Position}</td>
+              </tr>);
+            })}
+          </tbody>
+        </Table>
       </div>
     );
   }
--- frontend/util/stats_api_util.js
+++ frontend/util/stats_api_util.js
@@ -1,5 +1,5 @@
 const StatsApiUtil = {
-  getNbaStats(success) {
+  getNbaStats(typeOfStats, success) {
     // $.ajax({
     //   type: 'GET',
     //   url: 'https://api.fantasydata.net/nba/v2/JSON/Players HTTP/1.1',
@@ -13,7 +13,6 @@ const StatsApiUtil = {
     //     console.log(resp);
     //   }
     // });
-
     var settings = {
       "async": true,
       "crossDomain": true,
@@ -27,13 +26,44 @@ const StatsApiUtil = {
 
     $.ajax(settings).done(function (response) {
       // console.log(response);
-      success(response);
-      // return response;
+      success(typeOfStats, response);
+    }).fail(function () {
+      alert('API call failed');
+    });
+  },
+
+  getNflStats(typeOfStats, success) {
+    // $.ajax({
+    //   type: 'GET',
+    //   url: 'https://api.fantasydata.net/nba/v2/JSON/Players HTTP/1.1',
+    //   "headers": {
+    //     'Access-Control-Allow-Origin': 'http://localhost:3000',
+    //     'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+    //     'Access-Control-Allow-Headers': 'Content-type, Accept, X-Custom-Header',
+    //     'Ocp-Apim-Subscription-Key': '44b05285b729456db03ffcb6210e891d'
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api.fantasydata.net/v3/nfl/stats/JSON/Players",
+      "method": "GET",
+      "headers": {
+        "ocp-apim-subscription-key": "eac81d396d5549deb23e2458908d0ca7",
+        "cache-control": "no-cache"
+      }
+    };
+
+    $.ajax(settings).done(function (response) {
+      // console.log(response);
+      success(typeOfStats, response);
     }).fail(function () {
       alert('API call failed');
     });
   }
 };
-// Host: 'api.fantasydata.net',
 
 module.exports = StatsApiUtil;
