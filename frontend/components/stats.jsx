58c83347dbb7449b097d6b626745cba9f51be29e
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-10-06 16:29:50 -0700

    css added for stats page

--- frontend/components/stats.jsx
+++ frontend/components/stats.jsx
@@ -17,21 +17,32 @@ const Stats = React.createClass({
 
   render() {
     return (
-      <div className="stats-btns">
-        <Button onClick={this._redirectToNbaStats}
-              className="fetch-nba-btn">
-                NBA Stats
-        </Button>
-        <Button
-              className="fetch-nfl-btn"
-              onClick={this._redirectToNflStats}>
-                NFL Stats
-        </Button>
-        <Button to={'/stats/mlb'}
-              className="fetch-mlb-btn"
-              onClick={this._redirectToMlbStats}>
-                MLB Stats
-        </Button>
+      <div className="stats-container">
+        <p className="stats-paragraph">
+          Welcome to the stats page, where you can fetch stats about professional athletes based on the sport. Click one of the buttons below to get started!
+        </p>
+
+        <div className="stats-btns">
+          <Button onClick={this._redirectToNbaStats}
+                className="fetch-nba-btn">
+                  NBA Stats
+          </Button>
+          <Button
+                className="fetch-nfl-btn"
+                onClick={this._redirectToNflStats}>
+                  NFL Stats
+          </Button>
+          <Button to={'/stats/mlb'}
+                className="fetch-mlb-btn"
+                onClick={this._redirectToMlbStats}>
+                  MLB Stats
+          </Button>
+        </div>
+
+        <img className="stats-pic"
+          src="http://res.cloudinary.com/dcbb8bnvk/image/upload/v1475794979/sports_gkjlu2.png">
+        </img>
+
       </div>
   );}
 });
