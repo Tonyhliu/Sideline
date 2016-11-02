11857f5fb997f10f63cf6d0aedc23d1b5bc93088
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-11-02 12:20:12 -0700

    fixed modal blur

--- frontend/constants/modal_style.js
+++ frontend/constants/modal_style.js
@@ -1,12 +1,11 @@
 module.exports = {
   overlay : {
-    position          : 'absolute',
+    position          : 'fixed',
     top               : 0,
     left              : 0,
     right             : 0,
     bottom            : 0,
     backgroundColor   : 'rgba(255, 255, 255, 0.75)',
-    height            : '700px',
     overflow          : 'hidden'
   },
   content : {
